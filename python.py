74608e5721aac3791ad33bbcb9feee0926e195fb
Kowshi1101 <104734727+Kowshi1101@users.noreply.github.com>
2022-07-25 14:23:35 +0530

    Update python.py

--- python.py
+++ python.py
@@ -1,3 +1,10 @@
+# NAREN
 n=int(input("Enter a number:"))
 for i in range (2,n+n+2,2):
     print(i)
+
+#  KOWSHI
+n=int(input("enter the number"))
+for i  in range(n):
+    if i%2==0:
+        print(i)
